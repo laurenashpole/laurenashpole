dbbe32a72f7564a2389dae9f3094d7e32e29bb1c
laurenashpole <lashpole@thrillist.com>
2016-08-27 22:03:21 -0400

    html for emails

--- app/controllers/fonts.js
+++ app/controllers/fonts.js
@@ -164,8 +164,37 @@ exports.confirm = function (req, res) {
                         from: '"Lauren Ashpole" <lauren@laurenashpole.com>',
                         to: payment.payer.payer_info.email,
                         subject: 'Thank you for purchasing ' + font.name + '!',
-                        text: 'Here is your download!',
-                        html: 'Here is your download!',
+                        text:
+                            'Thanks for your purchase.\n\n' +
+                            'Your commercial font file is attached to this email. If you have any questions, don\'t hesitate to ask.\n\n' +
+                            'Thanks again!\n\nLauren\n\nwww.laurenashpole.com',
+                        html:
+                            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
+                            '<html xmlns="http://www.w3.org/1999/xhtml">' +
+                            '<head>' +
+                                '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+                                '<title></title>' +
+                                '<style></style>' +
+                            '</head>' +
+                            '<body>' +
+                                '<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="padding: 0 20px">' +
+                                    '<tr>' +
+                                        '<td align="center" valign="top">' +
+                                            '<table border="0" cellpadding="20" cellspacing="0" width="100%" style="max-width: 600px; background: #f3f2f2;">' +
+                                                '<tr>' +
+                                                    '<td valign="top">' +
+                                                         '<h1 style="text-align: center;">Thanks for your purchase.</h1>' +
+                                                         '<p>Your commercial font file is attached to this email. If you have any questions, don\'t hesitate to ask.</p>' +
+                                                         '<p>Thanks again!</p>' +
+                                                         '<p>Lauren<br />www.laurenashpole.com</p>' +
+                                                    '</td>' +
+                                                '</tr>' +
+                                            '</table>' +
+                                        '</td>' +
+                                    '</tr>' +
+                                '</table>' +
+                            '</body>' +
+                        '</html>',
                         attachments: [{
                             filename: font.commercial_font_file,
                             content: data
