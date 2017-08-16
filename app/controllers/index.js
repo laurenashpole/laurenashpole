d5516ece87bf3cce8e7b800988a898f95123e628
Lauren Ashpole <lashpole@hearst.com>
2017-08-16 14:38:15 -0400

    added about section, dynamic font header

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,14 @@
+var fontHelper = require('../helpers/fonts');
+
 exports.render = function (req, res) {
-    res.render('index', {
-        description: 'Custom, handcrafted fonts and dingbats for your personal and commercial projects. Plus, code snippets and themes.'
-    });
+    fontHelper.findBySlug('sewing-patterns-3')
+        .then (function (data) {
+            res.render('index', {
+                description: 'Custom, handcrafted fonts and dingbats for your personal and commercial projects. Plus, code snippets and themes.',
+                font: data.font
+            });
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 }
