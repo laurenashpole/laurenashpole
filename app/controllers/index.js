e66a5c373888fcc1edb825e321ee218572b8e15a
Lauren Ashpole <lauren@laurenashpole.com>
2018-04-03 12:09:44 -0400

    Check for Mail Chimp query string

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 let fontHelper = require('../helpers/fonts');
 
 exports.render = function (req, res) {
-  let hideEmailModal = req.query['referrer'] === 'email';
+  let hideEmailModal = req.query['mc_cid'] ? true : false;
 
   fontHelper.findAll()
     .then ((data) => {
