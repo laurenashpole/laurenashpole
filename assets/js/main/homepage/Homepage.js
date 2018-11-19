69468b1dce6d736a2609c2270f0aec14bfddf702
Lauren Ashpole <lauren@laurenashpole.com>
2018-11-19 16:15:24 -0500

    Added new font

--- assets/js/main/homepage/Homepage.js
+++ assets/js/main/homepage/Homepage.js
@@ -10,7 +10,7 @@ class Homepage extends Component {
     super(props);
 
     this.state = {
-      font: this.props.fonts.find((font) => font.slug === 'roundabout')
+      font: this.props.fonts.find((font) => font.slug === 'mistletoe')
     };
   }
 
