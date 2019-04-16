fc4e812928a4315707f986023f0c10195b06b922
Lauren Ashpole <lauren@laurenashpole.com>
2019-04-16 00:43:24 -0400

    Cleaned up distributors component, added new links

--- source/js/main/homepage/Homepage.js
+++ source/js/main/homepage/Homepage.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Cta from './components/Cta';
+import Hero from './components/Hero';
 import About from './components/About';
 import Distributors from './components/Distributors';
 
 const Homepage = (props) => {
   return(
     <main className="main">
-      <section className="hero--bg-repeat">
-        <div className="container container--large hero__container">
-          <div className="hero__img">
-            <img src={`/images/fonts/${props.font.image_main_retina}`} />
-          </div>
-
-          <Cta font={props.font} />
-        </div>
-      </section>
-
+      <Hero font={props.font} />
       <About />
       <Distributors />
     </main>
