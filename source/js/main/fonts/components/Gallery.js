6d7a9f597c047dfc1bd37868a348652ac4bfc88f
Lauren Ashpole <lauren@laurenashpole.com>
2019-10-17 01:19:11 -0400

    Added Pinterest button to font gallery images

--- source/js/main/fonts/components/Gallery.js
+++ source/js/main/fonts/components/Gallery.js
@@ -1,49 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-class Gallery extends Component {
-  constructor (props) {
-    super(props);
+const Gallery = ({ font }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    this.state = {
-      activeIndex: 0
-    };
-  }
+  useEffect(() => {
+    if (window.pinterestBuild) {
+      window.pinterestBuild();
+    }
+  }, []);
 
-  handleClick = (i) => {
-    this.setState({
-      activeIndex: i
-    });
-  }
+  return(
+    <section className="font__gallery">
+      <h2 className="font__heading text--uppercase u--center-mobile">Additional Images</h2>
 
-  render () {
-    const {
-      name,
-      image_collection,
-      image_collection_thumbnails
-    } = this.props.font;
+      <div className="font__gallery-main">
+        <span className="font__gallery-pin"><a href="https://www.pinterest.com/pin/create/button/" data-pin-do="buttonPin" data-pin-media={`/uploads/images/${font.image_collection[activeIndex]}`}></a></span>
+        <img src={`/uploads/images/${font.image_collection[activeIndex]}`} alt={font.name} />
+      </div>
 
-    return(
-      <section className="font__gallery">
-        <h2 className="font__heading text--uppercase u--center-mobile">Additional Images</h2>
-
-        <img src={`/uploads/images/${image_collection[this.state.activeIndex]}`} alt={name} />
-
-        {image_collection_thumbnails.length > 1 &&
-          <div className="font__gallery-thumbnails">
-            {image_collection_thumbnails.map((image, i) => {
-              return(
-                <div className="font__gallery-thumbnail" key={image}>
-                  <img src={`/uploads/images/${image}`} alt={`${name} Thumbnail`} onClick={this.handleClick.bind(this, i)} />
-                </div>
-              );
-            })}
-          </div>
-        }
-      </section>
-    );
-  }
-}
+      {font.image_collection_thumbnails.length > 1 &&
+        <div className="font__gallery-thumbnails">
+          {font.image_collection_thumbnails.map((image, i) => {
+            return(
+              <div className="font__gallery-thumbnail" key={image}>
+                <img src={`/uploads/images/${image}`} alt={`${font.name} Thumbnail`} onClick={() => setActiveIndex(i)} />
+              </div>
+            );
+          })}
+        </div>
+      }
+    </section>
+  );
+};
 
 Gallery.propTypes = {
   font: PropTypes.object
