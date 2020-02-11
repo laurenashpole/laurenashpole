534a4abaede244cf538fb9924a989581a3895696
Lauren Ashpole <lauren@laurenashpole.com>
2020-02-10 22:42:45 -0500

    Removed commented out code

--- app/routes/notfound.js
+++ app/routes/notfound.js
@@ -6,8 +6,6 @@ module.exports = function (app) {
 
     if (matches.length) {
       res.status(404);
-      // res.send('404: File Not Found');
-      res.status(404);
       return res.render('error', {
         message: 'Page not found.'
       });
@@ -15,30 +13,4 @@ module.exports = function (app) {
 
     next();
   });
-
-  // app.use((req, res, next) => {
-  //   let err = new Error('Oops!');
-  //   err.status = 404;
-  //   next(err);
-  // });
-
-  // // Development
-  // if (app.get('env') === 'development') {
-  //   app.use((err, req, res) => {
-  //     res.status(err.status || 500);
-  //     res.render('error', {
-  //       message: err.message,
-  //       error: err
-  //     });
-  //   });
-  // }
-
-  // // Production
-  // app.use((err, req, res) => {
-  //   res.status(err.status || 500);
-  //   res.render('error', {
-  //     message: err.message,
-  //     error: {}
-  //   });
-  // });
 };
