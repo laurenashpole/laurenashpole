8f4019542480bc5ac1bf9e1a8e280e314798c72c
Lauren Ashpole <lauren@laurenashpole.com>
2020-02-27 15:12:22 -0500

    Refactored tag helper

--- app/controllers/helpers/tags.js
+++ app/controllers/helpers/tags.js
@@ -5,22 +5,19 @@ exports.getTags = function (fonts) {
         const slug = tag.replace(/\s+/g, '-').toLowerCase();
         const name = tag[0].toUpperCase() + tag.substring(1);
 
+        const font = {
+          _id: cur._id,
+          name: cur.name,
+          slug: cur.slug,
+          image: cur.image
+        };
+
         if (acc[slug] && acc[slug].fonts) {
-          acc[slug].fonts.push({
-            _id: cur._id,
-            name: cur.name,
-            slug: cur.slug,
-            image: cur.image
-          });
+          acc[slug].fonts.push(font);
         } else {
           acc[slug] = {
             name: name,
-            fonts: [{
-              _id: cur._id,
-              name: cur.name,
-              slug: cur.slug,
-              image: cur.image
-            }]
+            fonts: [font]
           };
         }
       });
