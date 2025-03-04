76272baa3c92841572d5874e1a9630adbe1a64ee
laurenashpole <lauren@laurenashpole.com>
2025-03-04 02:23:44 -0500

    Swap nodemailer for sendgrid

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,23 +1,19 @@
-import { getTransporter } from '../../utils/mailers';
+import sgMail from '@sendgrid/mail';
 
 export default async (req, res) => {
   if (!req.body || !req.body.email || !req.body.name || !req.body.message) {
     return res.status(422).json({});
   }
 
-  const transporter = await getTransporter();
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  transporter.sendMail({
-    from: `"CONTACT FORM" <${process.env.EMAIL}>`,
+  sgMail.send({
     to: process.env.EMAIL,
+    from: `"CONTACT FORM" <${process.env.EMAIL}>`,
     subject: req.body.subject || 'General Questions',
     text: req.body.message,
-    html: `<p>Message from: ${req.body.email} (${req.body.name})</p><p>${req.body.message}</p>`
-  }, (err) => {
-    if (err) {
-      return res.status(500).json({});
-    }
-
-    res.json({});
-  });
+    html: `<p>Message from: ${req.body.email} (${req.body.name})</p><p>${req.body.message}</p>`,
+  })
+    .then(() => res.json({}))
+    .catch((err) => res.status(500).json({}));
 };
